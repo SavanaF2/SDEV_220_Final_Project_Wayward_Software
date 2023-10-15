c9cfa470c12075d1bb31f4a9f3e6c5db0522e288
Savana Fallen <113266360+SavanaF2@users.noreply.github.com>
2023-10-14 21:49:26 -0400

    Delete Group Project directory

--- Group Project/wayward_software/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Package, clientProfile
-
-from register.models import employeeProfile
-
-
-# Register your models here. 
-admin.site.register(Package)
-
--- Group Project/wayward_software/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path("logout", views.logout_request, name= "logout"),  
-     path('client_list/', views.client_list, name='client_list'),
-]
